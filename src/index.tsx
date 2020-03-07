5fef2b70256b8f0e88728542668785605f2b2fb6
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2020-03-07 13:49:29 +0900

    ESLint の設定を修正する

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App: React.FunctionComponent = () => {
-  return <h1>My React with Typescript App!</h1>;
+    return <h1>My React with Typescript App!</h1>;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
